41c3f9a43836a3e6cd668bb686f0ec2e3cf7d69c
Antonio Lopez <pezlop202@gmail.com>
2022-07-02 16:59:09 -0700

    updated navbar added mobile heading

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Main = () => {
   return (
-    <section>
-      <h1>WMW, the Shopify Plus agency for international brands.</h1>
+    <section className="bg-black py-6 text-white">
+      <div className="layout px-4">
+        <h1 className=" mt-[20vh] mb-9 text-3xl">
+          WMW, the Shopify Plus agency for international brands.
+        </h1>
+        <div className="mt-12 mb-4">
+          <div className="mr-6">
+            <Link href="/" passHref>
+              <a href="seewhy" className="inline-block border-b-2">
+                See why
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative mx-auto h-96 w-full md:max-w-7xl">
+        <Image
+          src="/images/WMW-Reel-Teaser-V10-Mobile.jpg"
+          layout="fill"
+          objectFit="contain"
+          alt="portfolio example"
+        />
+      </div>
     </section>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,10 @@ const Navbar = () => {
   return (
     <nav className="bg-black text-white">
       <div className="layout flex items-center justify-between py-10 px-6 text-sm font-bold italic">
-        <span>WE MAKE WEBSITES</span>
+        <div className="flex flex-col text-xl">
+          <span>WE MAKE</span>
+          <span className="ml-5">WEBSITES</span>
+        </div>
         <Image
           height={20}
           width={20}
