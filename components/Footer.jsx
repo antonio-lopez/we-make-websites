d8807b2d63187cc46a989ed690bee97ea56db68b
Antonio Lopez <pezlop202@gmail.com>
2022-07-03 23:30:13 -0700

    added footer mobile component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,56 @@
+import { citiesA, citiesB, resources, social } from '../constants/data';
+
 const Footer = () => {
-  return <div className="mt-[calc(5%_+_60px)]">Footer</div>;
+  return (
+    <div className="border-t-[1px] border-black/10 bg-white text-black">
+      <div className="layout">
+        <div className="grid grid-cols-2 gap-y-8 py-10 text-sm font-semibold md:grid-cols-4 md:text-base">
+          <span>
+            London <br />
+            90/92 Pentonville Rd <br />
+            London <br />
+            N1 9HS <br />
+            UK
+          </span>
+          <span>
+            New York <br />
+            114 W 26th St, Floor 2 <br />
+            New York <br />
+            10001 <br />
+            USA
+          </span>
+          <ul className="space-y-6">
+            {citiesA.map((item) => {
+              return <li key={item.id}>{item.city}</li>;
+            })}
+          </ul>
+          <ul className="space-y-6">
+            {citiesB.map((item) => {
+              return <li key={item.id}>{item.city}</li>;
+            })}
+          </ul>
+          <ul className="space-y-4">
+            {resources.map((item) => {
+              return <li key={item.id}>{item.city}</li>;
+            })}
+          </ul>
+          <ul className="space-y-4">
+            {social.map((item) => {
+              return <li key={item.id}>{item.city}</li>;
+            })}
+          </ul>
+        </div>
+
+        <div className="grid grid-cols-2 gap-y-8 py-10 text-sm font-semibold opacity-60 md:grid-cols-4 md:text-base">
+          <span className=" col-span-2">
+            © 2022 We Make Websites Ltd. A Tech Mahindra company.
+          </span>
+          <span>Privacy Policy</span>
+          <span>Accessibility</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- constants/data.js
+++ constants/data.js
@@ -321,3 +321,32 @@ export const blogData = [
     tag: 'strategy',
   },
 ];
+
+export const citiesA = [
+  { id: 0, city: 'Manchester' },
+  { id: 1, city: 'Cambridge' },
+  { id: 2, city: 'Vancouver' },
+  { id: 3, city: 'Toronto' },
+];
+
+export const citiesB = [
+  { id: 0, city: 'LA' },
+  { id: 1, city: 'Miami' },
+  { id: 2, city: 'Montreal' },
+];
+
+export const resources = [
+  { id: 0, city: 'Work' },
+  { id: 1, city: 'About' },
+  { id: 2, city: 'Blog' },
+  { id: 3, city: 'Resources' },
+  { id: 4, city: 'Glossary' },
+  { id: 5, city: 'Careers' },
+];
+
+export const social = [
+  { id: 0, city: 'LinkedIn' },
+  { id: 1, city: 'Twitter' },
+  { id: 2, city: 'Instagram' },
+  { id: 3, city: 'Shopify Plus Partner' },
+];
